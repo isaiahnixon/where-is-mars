0d57c5edffe13dbd00695412d9b265edf0aeb4a6
Isaiah Nixon <isaiahnixon@gmail.com>
2020-03-12 12:31:12 -0700

    Set up auto-refresh on ephemeris data.

--- js/main.js
+++ js/main.js
@@ -1,113 +1,79 @@
 (function() {
-	// Get the time and date.
-	var now = new Date();
+	/*** GLOBAL VARS ***/
+	const c = document.getElementById("solar-system");
+	const ctx = c.getContext("2d");
 
-	// Instantiate ephemeris.
-	const ephemeris = new Ephemeris.default({
-	  year: now.getFullYear(),
-	  month: (now.getMonth()+1),
-	  day: now.getDate(),
-	  hours: now.getHours(),
-	  minutes: now.getMinutes(),
-	  latitude: 41.37,
-	  longitude: -71.1,
-	  calculateShadows: false
-	});
+	/*** FUNCTIONS ***/
+	const getEphemeris = () => {
+		// Get the time and date.
+		const now = new Date();
 
-	// Populate heliocentric coordinates.
-	$('#mars .x').text(ephemeris.mars.position.rect[0]);
-	$('#mars .y').text(ephemeris.mars.position.rect[1]);
-	$('#mars .z').text(ephemeris.mars.position.rect[2]);
+		// Instantiate ephemeris.
+		return new Ephemeris.default({
+		  year: now.getFullYear(),
+		  month: (now.getMonth()+1),
+		  day: now.getDate(),
+		  hours: now.getHours(),
+		  minutes: now.getMinutes(),
+		  latitude: 41.37,
+		  longitude: -71.1,
+		  calculateShadows: false
+		});
+	};
 
-	$('#earth .x').text(ephemeris.Earth.position.rect[0]);
-	$('#earth .y').text(ephemeris.Earth.position.rect[1]);
-	$('#earth .z').text(ephemeris.Earth.position.rect[2]);
+	const updateDisplayedData = (ephemeris) => {
+		// Populate heliocentric coordinates.
+		$('#mars .x').text(ephemeris.mars.position.rect[0]);
+		$('#mars .y').text(ephemeris.mars.position.rect[1]);
+		$('#mars .z').text(ephemeris.mars.position.rect[2]);
 
-	// Populate the geocentric distance.
-	$('#distance').text(ephemeris.mars.position.geocentricDistance);
+		$('#earth .x').text(ephemeris.Earth.position.rect[0]);
+		$('#earth .y').text(ephemeris.Earth.position.rect[1]);
+		$('#earth .z').text(ephemeris.Earth.position.rect[2]);
 
-	// Draw solar system.
-	drawSolarSystem();
-	$(window).on('resize', drawSolarSystem);
+		// Populate the geocentric distance.
+		$('#distance').text(ephemeris.mars.position.geocentricDistance);
+	}
 
-	// Function for drawing the solar system on the canvas.
-	function drawSolarSystem() {
-		// Get the dimensions of the canvas.
-		var c_width = $('#solar-system').width();
-		var c_height = $('#solar-system').height();
-		var min = Math.min(c_height, c_width);
+	// Source: https://medium.com/better-programming/fun-with-html-canvas-lets-create-a-star-field-a46b0fed5002
+	const putPixel = (x, y, brightness) => {
+	  	const intensity = brightness * 255;
+	  	const rgb = "rgb(" + intensity + "," + intensity + "," + intensity + ")";
+	  	ctx.fillStyle = rgb;
+	  	ctx.fillRect(x, y, 1, 1);
+	};
 
-		// Instantiate the canvas context.
-		var c = document.getElementById("solar-system");
-		c.width = c_width;
-		c.height = c_height;
-		var ctx = c.getContext("2d");
+	// Based on: https://stackoverflow.com/questions/4959975
+	const randomWithMax = (max) => {
+  		return Math.floor(Math.random() * max) + 1 
+	}
 
-		// Function for making stars.
-		// Source: https://medium.com/better-programming/fun-with-html-canvas-lets-create-a-star-field-a46b0fed5002
-		const putPixel = (x, y, brightness) => {
-		  	const intensity = brightness * 255;
-		  	const rgb = "rgb(" + intensity + "," + intensity + "," + intensity + ")";
-		  	ctx.fillStyle = rgb;
-		  	ctx.fillRect(x, y, 1, 1);
-		};
-
-		// Function for random numbers between set values.
-		// Based on: https://stackoverflow.com/questions/4959975
-		const randomWithMax = (max) => {
-	  		return Math.floor(Math.random() * max) + 1 
-		}
-
+	const drawEphemerisDependentComponents = (ephemeris) => {
 		// Crunch some numbers.
-		var mars = {
+		const min = Math.min(c.height, c.width);
+
+		const mars = {
 			distance: Math.sqrt(Math.pow(ephemeris.mars.position.rect[0], 2) + Math.pow(ephemeris.mars.position.rect[1], 2)),
-			x: c_width/2 + (min/3 * ephemeris.mars.position.rect[0]),
-			y: c_height/2 + (min/3 * ephemeris.mars.position.rect[1])
+			x: c.width/2 + (min/3 * ephemeris.mars.position.rect[0]),
+			y: c.height/2 + (min/3 * ephemeris.mars.position.rect[1])
 		};
 
-		var earth = {
+		const earth = {
 			distance: Math.sqrt(Math.pow(ephemeris.Earth.position.rect[0], 2) + Math.pow(ephemeris.Earth.position.rect[1], 2)),
-			x: c_width/2 + (min/3 * ephemeris.Earth.position.rect[0]),
-			y: c_height/2 + (min/3 * ephemeris.Earth.position.rect[1])
+			x: c.width/2 + (min/3 * ephemeris.Earth.position.rect[0]),
+			y: c.height/2 + (min/3 * ephemeris.Earth.position.rect[1])
 		};
-
-		// Generate stars
-		let stars = [];
-		for (var i = 0; i < 200; i++) {
-			stars[i] = {
-				x: randomWithMax(c_width),
-				y: randomWithMax(c_height),
-				brightness: Math.random()
-			};
-		}
-
-		// Make the background.
-		ctx.fillStyle = "#222";
-		ctx.fillRect(0, 0, c_width, c_height);
-
-		// Draw the stars.
-		stars.forEach(star => putPixel(star.x, star.y, star.brightness));
-
-		// Set the stars to flicker.
-		const flickerStarsInterval = setInterval(() => {
-			stars.forEach(star => {
-				if (Math.random() < 0.025) {
-					putPixel(star.x, star.y, Math.random());
-				}
-			});
-		}, 100);
-		$(window).resize(() => clearInterval(flickerStarsInterval));
 
 		// Make mars orbit.
 		ctx.strokeStyle = "#00FF00";
 		ctx.beginPath();
-		ctx.arc((c_width/2), (c_height/2), (min/3 * mars.distance), 0, 2 * Math.PI);
+		ctx.arc((c.width/2), (c.height/2), (min/3 * mars.distance), 0, 2 * Math.PI);
 		ctx.stroke();
 
 		// Make earth orbit.
 		ctx.strokeStyle = "#00FF00";
 		ctx.beginPath();
-		ctx.arc((c_width/2), (c_height/2), (min/3 * earth.distance), 0, 2 * Math.PI);
+		ctx.arc((c.width/2), (c.height/2), (min/3 * earth.distance), 0, 2 * Math.PI);
 		ctx.stroke();
 
 		// Make line between them.
@@ -120,7 +86,7 @@
 		// Make the sun.
 		ctx.fillStyle = "#FFAA1D";
 		ctx.beginPath();
-		ctx.arc((c_width/2), (c_height/2), (min * 0.04), 0, 2 * Math.PI);
+		ctx.arc((c.width/2), (c.height/2), (min * 0.04), 0, 2 * Math.PI);
 		ctx.fill();
 
 		// Make the earth.
@@ -148,7 +114,7 @@
 		// Draw sun caption.
 		ctx.font = "bold 12px Arial";
 		ctx.fillStyle = '#FFAA1D';
-		ctx.fillText("Sun", (c_width/2 + min * 0.04), (c_height/2 - min * 0.04));		
+		ctx.fillText("Sun", (c.width/2 + min * 0.04), (c.height/2 - min * 0.04));		
 
 		// Crunch some more numbers.
 		var slope =  (earth.y - mars.y) / (earth.x - mars.x);
@@ -165,7 +131,73 @@
 		ctx.fillStyle = '#9400D3';
 		ctx.fillText(ephemeris.mars.position.geocentricDistance.toFixed(3) + " AU", (min * -0.016), (min * -0.016));
 		ctx.restore();
-	}
-	
+	};
 
+	const setUpCanvas = () => {
+		// Get the dimensions of the canvas.
+		const c_width = $('#solar-system').width();
+		const c_height = $('#solar-system').height();
+		const min = Math.min(c_height, c_width);
+
+		// Instantiate the canvas context.
+		c.width = c_width;
+		c.height = c_height;
+
+		// Make the background.
+		ctx.fillStyle = "#222";
+		ctx.fillRect(0, 0, c_width, c_height);
+	};
+
+	const generateStars = () => {
+		let stars = [];
+		for (var i = 0; i < 200; i++) {
+			stars[i] = {
+				x: randomWithMax(c.width),
+				y: randomWithMax(c.height),
+				brightness: Math.random()
+			};
+		}
+
+		return stars;
+	}
+
+	const drawStars = (stars) => {
+		// Draw the stars.
+		stars.forEach(star => putPixel(star.x, star.y, star.brightness));
+
+		// Set the stars to flicker.
+		const flickerStarsInterval = setInterval(() => {
+			stars.forEach(star => {
+				if (Math.random() < 0.025) {
+					putPixel(star.x, star.y, Math.random());
+				}
+			});
+		}, 100);
+		$(window).resize(() => clearInterval(flickerStarsInterval));
+	};
+
+	/*** RUNTIME CODE ***/
+	let ephemeris = getEphemeris();
+	updateDisplayedData(ephemeris);
+	setUpCanvas();
+	let stars = generateStars();
+	drawStars(stars);
+	drawEphemerisDependentComponents(ephemeris);
+	
+	$(window).on('resize', () => {
+		ctx.clearRect(0, 0, c.width, c.height);
+		setUpCanvas();
+		stars = generateStars();
+		drawStars(stars);
+		drawEphemerisDependentComponents(ephemeris);
+	});
+
+	setInterval(() => {
+		ephemeris = getEphemeris();
+		updateDisplayedData(ephemeris);
+		ctx.clearRect(0, 0, c.width, c.height);
+		setUpCanvas();
+		drawStars(stars);
+		drawEphemerisDependentComponents(ephemeris);
+	}, 60000);
 }());
